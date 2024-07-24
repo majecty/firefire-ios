0b70236677b7069390c38ccdbd2baa128c5203b8
juhyung <me@juhyung.dev>
2024-07-24 23:34:41 +0900

    성공 하지만 영상이 위아래가 바뀜

--- VideoEffect/ScenekitTest/ScenekitTest.swift
+++ VideoEffect/ScenekitTest/ScenekitTest.swift
@@ -10,12 +10,15 @@ import SwiftUI
 import SceneKit
 import AVKit
 import SpriteKit
+import CoreMotion
 
 class VideoPlayerViewModel: ObservableObject {
     private static let defaultURL = Bundle.main.url(forResource: "mv200_2s", withExtension: "mp4")!
     let player = AVQueuePlayer(url: defaultURL)
     let playerLooper: AVPlayerLooper
     let videoNode: SKVideoNode
+    
+    let motionManager: CMMotionManager = CMMotionManager()
 //    let cameraNode: SCNNode
     
     func createSphereNode(material: AnyObject?) -> SCNNode {
@@ -28,6 +31,8 @@ class VideoPlayerViewModel: ObservableObject {
     }
     
     @Published var scene = SCNScene(named: "video360.scn")
+    
+    let cameraNode = SCNNode()
 
     init () {
         let asset = AVAsset(url: VideoPlayerViewModel.defaultURL)
@@ -48,30 +53,44 @@ class VideoPlayerViewModel: ObservableObject {
         let sphereNode = self.createSphereNode(material: spriteScene)
         scene?.rootNode.addChildNode(sphereNode)
         
-        let cameraNode = SCNNode()
+//        let cameraNode = SCNNode()
         cameraNode.name = "camera"
         cameraNode.camera = SCNCamera()
         cameraNode.position = SCNVector3Make(0, 0, 0)
         scene?.rootNode.addChildNode(cameraNode)
+        
+        
+        self.motionManager.deviceMotionUpdateInterval = 1.0 / 60
+        
+        self.motionManager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical, to: .main) {
+            [weak self] (motionData, error) in
+            guard error == nil else {
+                print (error!)
+                return
+            }
+            
+            guard let self = self else {
+                return
+            }
+            
+            guard let motionData = motionData else {
+                return
+            }
+            
+            self.cameraNode.orientation = motionData.gaze(atOrientation: .landscapeLeft)
+//            let attitude = motionData.attitude
+//            self.cameraNode
+//            self.cameraNode.eulerAngles = SCNVector3Make(Float(attitude.roll - Double.pi/2.0), Float(attitude.yaw), Float(attitude.pitch))
+        }
     }
 }
 
 struct ScenekitTest: View {
-//    let scene = SCNScene(named: "360videoScene")
-//    var cameraNode: SCNNode? {
-//        scene?.rootNode.childNode(withName: "camera", recursively: false)
-//    }
     var cameraNode: SCNNode? {
         model.scene?.rootNode.childNode(withName: "camera", recursively: false)
     }
 
-
     @StateObject var model = VideoPlayerViewModel()
-    
-//    init() {
-//        model = VideoPlayerViewModel(scene: scene)
-//    }
-
     
     var body: some View {
         SceneView(
@@ -81,5 +100,57 @@ struct ScenekitTest: View {
                 SceneView.Options.allowsCameraControl,
             ]
         )
+    }
+}
+
+extension CMDeviceMotion {
+    
+    func gaze(atOrientation orientation: UIInterfaceOrientation) -> SCNVector4 {
+        
+        let attitude = self.attitude.quaternion
+        let aq = GLKQuaternionMake(Float(attitude.x), Float(attitude.y), Float(attitude.z), Float(attitude.w))
+        
+        let final: SCNVector4
+        
+        switch orientation {
+            
+        case .landscapeRight:
+            
+            let cq = GLKQuaternionMakeWithAngleAndAxis(Float.pi / 2, 0, 1, 0)
+            let q = GLKQuaternionMultiply(cq, aq)
+            
+            final = SCNVector4(x: -q.y, y: q.x, z: q.z, w: q.w)
+            
+        case .landscapeLeft:
+            
+            let cq = GLKQuaternionMakeWithAngleAndAxis(-Float.pi / 2, 0, 1, 0)
+            let q = GLKQuaternionMultiply(cq, aq)
+            
+            final = SCNVector4(x: q.y, y: -q.x, z: q.z, w: q.w)
+            
+        case .portraitUpsideDown:
+            
+            let cq = GLKQuaternionMakeWithAngleAndAxis(Float.pi / 2, 1, 0, 0)
+            let q = GLKQuaternionMultiply(cq, aq)
+            
+            final = SCNVector4(x: -q.x, y: -q.y, z: q.z, w: q.w)
+            
+        case .unknown:
+            
+            fallthrough
+            
+        case .portrait:
+            
+            fallthrough
+            
+        @unknown default:
+            
+            let cq = GLKQuaternionMakeWithAngleAndAxis(-Float.pi / 2, 1, 0, 0)
+            let q = GLKQuaternionMultiply(cq, aq)
+            
+            final = SCNVector4(x: q.x, y: q.y, z: q.z, w: q.w)
+        }
+        
+        return final
     }
 }
